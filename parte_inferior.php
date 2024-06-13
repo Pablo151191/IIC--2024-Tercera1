16abcff451a8eecf2942161a6651a1cbca4dc0e7
Pablo <pablojimenezb24@gmail.com>
2024-06-12 21:47:54 -0600

    Primera Clase

---
+++ parte_inferior.php
@@ -0,0 +1,1 @@
+<?php require_once "./parte_inferior.php" ?>
---
+++ parte_superior.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Proyecto de Tecera 1" content="Pagina de prueba">
+    <title>
+        Tercera 1
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+<menu>
+    <div class="menu-izquierda">
+        <a href= ./"index.php">
+
+        </a>
+        <img src="./images/menu.png" alt="">
+        <p class="texto-menu-izquierda"> 
+            Mario Bross
+            
+        </p>
+    </div>
+</menu>
+
+</html>
