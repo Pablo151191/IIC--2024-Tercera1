1c7545f4078764097bf21a7e3a4e39eda3430a98
Pablo <pablojimenezb24@gmail.com>
2024-07-03 20:27:41 -0600

    Ultima clase

--- parte_inferior.php
+++ parte_inferior.php
@@ -1,3 +1,18 @@
+<footer>
+
+    <div class="footer-izquierda">
+        <p>Tercera 1, II Cuatrimestre, 2024</p>
+    </div>
+
+    <div class="footer-derecha">
+        <p>Todos los derechos reservados</p>
+        <p>Esta es una pagina de prueba</p>
+        <p>2024-2025</p>
+    </div>
+
+</footer>
+
+
 <script src="./script/hamburger-icon.js"></script>
 <script src="./script/accordion.js"></script>
 <script src="./script/cards.js"></script>
